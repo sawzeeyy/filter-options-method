f0ad60c46c052d04c7951132e8d59a3da8a9ddf6
Shuaib Oladigbolu <saoladigbolu@gmail.com>
2020-05-19 05:32:43 +0100

    Replace try block with an if statement

--- filter-options-method.py
+++ filter-options-method.py
@@ -2,7 +2,7 @@
 Name:           Filter OPTIONS Method
 Version:        1.2
 Author:         Capt. Meelo (@CaptMeelo)
-Description:    A Burp extension that filters out `OPTIONS` requests from populating Burp's Proxy history. 
+Description:    A Burp extension that filters out `OPTIONS` requests from populating Burp's Proxy history.
 Blog Post:      https://captmeelo.com/pentest/2020/01/06/filter-options-method.html
 Idea From:      https://parsiya.net/blog/2019-04-06-hiding-options-an-adventure-in-dealing-with-burp-proxy-in-an-extension/ (Huge Thanks!)
 """
@@ -10,23 +10,24 @@ Idea From:      https://parsiya.net/blog/2019-04-06-hiding-options-an-adventure-
 from burp import IBurpExtender
 from burp import IHttpListener
 
+
 class BurpExtender(IBurpExtender, IHttpListener):
 
     # implement IBurpExtender
     def registerExtenderCallbacks(self, callbacks):
-        
+
         # obtain an extension helpers object
         self._helpers = callbacks.getHelpers()
-        
+
         # set our extension name
         callbacks.setExtensionName("Filter OPTIONS Method")
-        
+
         # register ourselves as an HTTP listener
         callbacks.registerHttpListener(self)
 
     # implement IHttpListener
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
-        
+
         # only process responses
         if messageIsRequest:
             return
@@ -52,17 +53,16 @@ class BurpExtender(IBurpExtender, IHttpListener):
             for headers in responseHeaders:
                 if "Content-Type: " in headers:
                     removeHeaders = headers
-            try:
+
+            if removeHeaders:
                 responseHeaders.remove(removeHeaders)
-            except:
-                pass
 
             # add the new Content-Type
             responseHeaders.add("Content-Type: text/css; charset=UTF-8")
-            
+
             # comment out the response body (refer to the blog why I did it)
             responseBodyBytes = "/* Injected by 'Filter OPTIONS Method'\n\n" + responseBytes[responseInfo.getBodyOffset():] + "\n\nInjected by 'Filter OPTIONS Method' */"
-            
+
             # rebuild the new response
             responseModified = self._helpers.buildHttpMessage(responseHeaders, responseBodyBytes)
             messageInfo.setResponse(responseModified)
